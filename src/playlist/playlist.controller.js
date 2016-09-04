6b29962f7c46f058a3d208c7dc8d036551b6c543
Tyler Reisinger <reisinger.tyler@gmail.com>
2016-09-04 21:23:57 +0000

    Initial population of tracklist

--- src/playlist/playlist.controller.js
+++ src/playlist/playlist.controller.js
@@ -9,9 +9,15 @@
         var vm = this;
 
         var moppy = new mopidyService();
+
+        vm.tracks = [];
+
         var q = moppy.rpc("core.tracklist.slice", [0, 100]).then(function (msg) {
             console.log('Yay');
             console.log(msg);
+            for(var tl of msg.result) {
+                vm.tracks.push(tl.track);
+            }
         }).catch(function (err) {
             console.log('Uh oh!');
             console.log(err);
