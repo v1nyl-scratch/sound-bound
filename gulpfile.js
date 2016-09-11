91befd1296bd10895f14aa6619e3b904f15e83db
Tyler Reisinger <reisinger.tyler@gmail.com>
2016-09-11 18:18:32 +0000

    gulp workflow implemented. "gulp deploy" should assemble all sources and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var shell = require('gulp-shell');
+var inject = require('gulp-inject');
 
-gulp.task('default', function () {
-    return gutil.log('Hello, world!');
+gulp.task('scss', function () {
+    return gulp.src('./src/app.scss').pipe(sass()).pipe(gulp.dest('./build/src/'));
 });
+
+gulp.task('inject', function () {
+    var sources = gulp.src('./src/**/*.js', {read: false});
+    var inject_cfg = {addRootSlash: false};
+
+    return gulp.src('./src/index.html')
+        .pipe(inject(
+            gulp.src('./src/**/*.module.js', {read: false}), 
+                {addRootSlash: false, name: 'inject:modules'}))
+        .pipe(inject(
+            gulp.src(['./src/**/*.js', '!./src/**/*.module.js'], {read: false}), inject_cfg))
+        .pipe(gulp.dest('./build/src/'));
+});
+
+gulp.task('package-metadata', function () {
+    return gulp.src('./package.json').pipe(gulp.dest('./build/'));
+});
+
+gulp.task('package-bower', function () {
+    return gulp.src('./bower_components/**/*').pipe(gulp.dest('./build/bower_components/'));
+});
+
+gulp.task('package', ['scss', 'inject', 'package-bower', 'package-metadata'], function () {
+    return gulp.src(['./src/**/*.html', './src/**/*.js', '!./src/index.html']).pipe(gulp.dest('./build/src/'));
+});
+
+gulp.task('deploy', ['package'], shell.task(['echo "Deploying"', 'deploy-dev ./build']));
--- src/playlist/playlist.module.js
+++ src/playlist/playlist.module.js
@@ -1,6 +1,9 @@
 (function () {
+    'use strict';
+
     angular
         .module('sound_bound.playlist', [
+            'sound_bound',
             'sound_bound.mopidy'
         ]);
 })();
